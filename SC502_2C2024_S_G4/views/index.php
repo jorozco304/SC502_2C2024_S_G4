dd9a5783901cdf4f4bda7c5bf428cfd8fa26065c
Brandonure10 <138939315+Brandonure10@users.noreply.github.com>
2024-07-19 20:04:25 -0600

    Frond end 50%

--- SC502_2C2024_S_G4/views/index.php
+++ SC502_2C2024_S_G4/views/index.php
@@ -28,20 +28,6 @@
 <body>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
-  <div class="container">
-        <form action="../index.php" method="post">
-            <div class="mb-3">
-                <label for="username" class="form-label">Nombre Usuario</label>
-                <input type="text" class="form-control" name="username" id="username" required>
-            </div>
-            <div class="mb-3">
-                <label for="clave" class="form-label">Clave</label>
-                <input type="password" class="form-control" name="clave" id="clave" required>
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
-    </div>
-
   <header>
     <?php
     include 'header.php'
